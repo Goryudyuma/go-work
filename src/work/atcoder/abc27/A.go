357f932c6d92a4f33a65c930d3eb65c0a0ef756d
Goryudyuma <umaretekyoumade@gmail.com>
2015-08-09 00:16:46 +0900

    abc27A追加

---
+++ src/work/atcoder/abc27/A.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	i, e := strconv.Atoi(nextString())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nextFloat() float64 {
+	f, e := strconv.ParseFloat(nextString(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func nextString() string {
+	sc.Split(bufio.ScanWords)
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	a, b, c := nextInt(), nextInt(), nextInt()
+	if a == b {
+		fmt.Println(c)
+	} else if a == c {
+		fmt.Println(b)
+	} else {
+		fmt.Println(a)
+	}
+}
