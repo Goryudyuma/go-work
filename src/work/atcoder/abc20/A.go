c940f68dcfe79439d0893fb7d96c1fc0554e0d57
Goryudyuma <umaretekyoumade@gmail.com>
2015-08-19 04:42:24 +0900

    atcoder一問追加。
    ムーンライトながら車内より

---
+++ src/work/atcoder/abc20/A.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	i, e := strconv.Atoi(nextString())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nextInt64() int64 {
+	i, e := strconv.ParseInt(nextString(), 10, 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+
+}
+
+func nextFloat() float64 {
+	f, e := strconv.ParseFloat(nextString(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func nextString() string {
+	sc.Split(bufio.ScanWords)
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	Q := nextInt()
+	switch Q {
+	case 1:
+		fmt.Println("ABC")
+	case 2:
+		fmt.Println("chokudai")
+	}
+}
