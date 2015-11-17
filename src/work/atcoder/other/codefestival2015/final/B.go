3002614d6ae1e766a4a022b902cd564163e0bad0
Goryudyuma <umaretekyoumade@gmail.com>
2015-11-17 15:43:45 +0900

    atcoder一問追加

---
+++ src/work/atcoder/other/codefestival2015/final/B.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	i, e := strconv.Atoi(nextString())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nextInt64() int64 {
+	i, e := strconv.ParseInt(nextString(), 10, 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+
+}
+
+func nextFloat() float64 {
+	f, e := strconv.ParseFloat(nextString(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func nextString() string {
+	sc.Split(bufio.ScanWords)
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	N := nextInt()
+	if N == 1 {
+		fmt.Println(1)
+	} else {
+		fmt.Println((int)(float64(N) * 3.5))
+	}
+}
