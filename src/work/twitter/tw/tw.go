322446e8170a2dce58453861fde4ac7ffd96fa79
Goryudyuma <umaretekyoumade@gmail.com>
2015-11-19 11:51:43 +0900

    tw.goをgolintで怒られないように修正した

--- src/work/twitter/tw/tw.go
+++ src/work/twitter/tw/tw.go
@@ -26,17 +26,17 @@ func access(fp *os.File) *anaconda.TwitterApi {
 		panic(err)
 	}
 	scanner.Scan()
-	consumer_key := scanner.Text()
+	consumerKey := scanner.Text()
 	scanner.Scan()
-	consumer_secret := scanner.Text()
+	consumerSecret := scanner.Text()
 	scanner.Scan()
 	accesstoken := scanner.Text()
 	scanner.Scan()
-	accesstoken_secret := scanner.Text()
+	accesstokenSecret := scanner.Text()
 
-	anaconda.SetConsumerKey(consumer_key)
-	anaconda.SetConsumerSecret(consumer_secret)
-	return anaconda.NewTwitterApi(accesstoken, accesstoken_secret)
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+	return anaconda.NewTwitterApi(accesstoken, accesstokenSecret)
 }
 
 func reverse(s string) string {
@@ -49,9 +49,9 @@ func reverse(s string) string {
 
 //ツイート関数。無駄に並列化している。140字以上の分割ツイートにも対応。
 func tweet(api *anaconda.TwitterApi, waitGroup *sync.WaitGroup, begin string, end string, str string) {
-	s_len := 140 - utf8.RuneCountInString(begin) - utf8.RuneCountInString(end)
+	sLen := 140 - utf8.RuneCountInString(begin) - utf8.RuneCountInString(end)
 	for str != "" {
-		limit := int(math.Min(float64(s_len), float64(utf8.RuneCountInString(str))))
+		limit := int(math.Min(float64(sLen), float64(utf8.RuneCountInString(str))))
 		waitGroup.Add(1)
 		go func(api *anaconda.TwitterApi, waitGroup *sync.WaitGroup, begin, end, str string) {
 			defer waitGroup.Done()
@@ -73,7 +73,7 @@ func tweet(api *anaconda.TwitterApi, waitGroup *sync.WaitGroup, begin string, en
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	api_chan := make(chan *anaconda.TwitterApi)
+	apiChan := make(chan *anaconda.TwitterApi)
 
 	go func() {
 		fp, err := os.Open("/etc/pass/twi.txt")
@@ -82,25 +82,25 @@ func main() {
 		}
 		defer fp.Close()
 
-		api_chan <- access(fp)
+		apiChan <- access(fp)
 	}()
 
 	//引数処理
-	var b_string = flag.String("b", "", "ツイートの最初に添加する文字列")
-	var e_string = flag.String("e", "", "ツイートの最後に添加する文字列")
+	var bString = flag.String("b", "", "ツイートの最初に添加する文字列")
+	var eString = flag.String("e", "", "ツイートの最後に添加する文字列")
 	var c = flag.Bool("c", false, "連投モードに突入")
 	r = flag.Bool("r", false, "リバースモードに突入")
 	h = flag.Bool("h", false, "ハッシュモードに突入")
 	flag.Parse()
 
-	begin := *b_string + " "
-	end := " " + *e_string
+	begin := *bString + " "
+	end := " " + *eString
 
 	if utf8.RuneCountInString(begin)+utf8.RuneCountInString(end) >= 140 {
 		fmt.Println("beginとendを合わせた文字列が138字未満になるようにしてください。")
 		return
 	}
-	api := <-api_chan
+	api := <-apiChan
 
 	var waitGroup sync.WaitGroup
 	if *c {
