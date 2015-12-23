c0f2a4ef7c2920460850af7432ea6b2d6d3f0dda
Goryudyuma <umaretekyoumade@gmail.com>
2015-12-23 20:15:58 +0900

    atcoder一問追加

---
+++ src/work/atcoder/arc/arc046/A.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	i, e := strconv.Atoi(nextString())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nextInt64() int64 {
+	i, e := strconv.ParseInt(nextString(), 10, 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+
+}
+
+func nextFloat() float64 {
+	f, e := strconv.ParseFloat(nextString(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func nextString() string {
+	sc.Split(bufio.ScanWords)
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	N := nextInt()
+	N--
+	for i := -1; i < N/9; i++ {
+		fmt.Printf("%d", N%9+1)
+	}
+	fmt.Println()
+}
