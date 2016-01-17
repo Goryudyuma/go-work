c8a9aee30911f1f976c533322ad6bb1c37fb2100
Goryudyuma <umaretekyoumade@gmail.com>
2016-01-18 08:32:37 +0900

    atcoder一問追加

---
+++ src/work/atcoder/abc/abc32/B.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	i, e := strconv.Atoi(nextString())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nextInt64() int64 {
+	i, e := strconv.ParseInt(nextString(), 10, 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+
+}
+
+func nextFloat() float64 {
+	f, e := strconv.ParseFloat(nextString(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func nextString() string {
+	sc.Split(bufio.ScanWords)
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	S, K := nextString(), nextInt()
+	M := make(map[string]int, 0)
+	for i := 0; i <= len(S)-K; i++ {
+		M[S[i:i+K]] = 1
+	}
+	fmt.Println(len(M))
+}
